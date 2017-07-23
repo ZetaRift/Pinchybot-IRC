aedecc5cb37013ec0347d2edb0acd6615cf13af0
ZetaRift <zetarift@gmail.com>
2017-07-23 13:54:26 -0400

    Tab identcation

--- main.py
+++ main.py
@@ -2,37 +2,39 @@
 
 import os
 import sys
+import traceback
 
 
 def pyvercheck():
- if sys.version_info < (3, 0):
-  print("Requires python 3.x, exiting.")
-  sys.exit(1)
+	if sys.version_info < (3, 0):
+		print("Requires python 3.x, exiting.")
+		sys.exit(1)
 
 def run():
- pyvercheck()
- print("Child PID: "+str(os.getpid()))
- try:
-  import ircbot
- except ImportError:
-  print("Cannot find ircbot module, exiting")
-  sys.exit(1)
- bot = ircbot.IRCBot()
- bot.connect()
+	pyvercheck()
+	print("Child PID: "+str(os.getpid()))
+	try:
+		import ircbot
+	except ImportError:
+		print(traceback.format_exc())
+		print("Cannot find ircbot module, exiting")
+		sys.exit(1)
+	bot = ircbot.IRCBot()
+	bot.connect()
 
 
 
 
 
 if __name__ == "__main__":
- print("Master PID:"+str(os.getpid()))
- do_fork = True
- while do_fork:
-  cpid = os.fork()
-  if cpid == 0:
-   run()
-  pid, status = os.waitpid(cpid, 0)
-  if status == 1280:
-   print("Restarting child process..")
-  print(status)
-  do_fork = (status == 1280)
+	print("Master PID:"+str(os.getpid()))
+	do_fork = True
+	while do_fork:
+		cpid = os.fork()
+		if cpid == 0:
+			run()
+		pid, status = os.waitpid(cpid, 0)
+		if status == 1280 or 256:
+			print("Restarting child process..")
+		print(status)
+		do_fork = (status == 1280 or status == 256)
